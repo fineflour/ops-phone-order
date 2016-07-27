e1c8c497d2bc3138e9f18ed50eeecbcc278d39c9
fineflour <soojin.cho@bfa.org>
2016-07-27 12:09:41 -0700

    add all question mp3 filess to be played

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -20,7 +20,7 @@ class TwilioController < ApplicationController
   def first_name
     response = Twilio::TwiML::Response.new do |r|
       r.Play "http://75.119.204.130/ivr/english_prompts/STE-020.mp3"
-      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', RecordingUrl: params['RecordingUrl'], action: last_name_path     
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: last_name_path     
     end
     render text: response.text
   end
@@ -29,87 +29,67 @@ class TwilioController < ApplicationController
   def last_name
     response = Twilio::TwiML::Response.new do |r|
       r.Play "http://75.119.204.130/ivr/english_prompts/STE-021.mp3"
-      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', RecordingUrl: params['RecordingUrl']     
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: zipcode_path
     end
     render text: response.text
   end
 
-  # GET ivr/selection
-  def menu_selection
-    user_selection = params[:Digits]
-
-    case user_selection
-    when "1"
-      @output = "To get to your extraction point, get on your bike and go down
-        the street. Then Left down an alley. Avoid the police cars. Turn left
-        into an unfinished housing development. Fly over the roadblock. Go
-        passed the moon. Soon after you will see your mother ship."
-      twiml_say(@output, true)
-    when "2"
-      list_planets
-    else
-      @output = "Returning to the main menu."
-      twiml_say(@output)
-    end
-
-  end
-
-  # POST/GET ivr/planets 
-  # planets_path
-  def planet_selection
-    user_selection = params[:Digits]
-
-    case user_selection
-    when "2"
-      twiml_dial("+12024173378")
-    when "3"
-      twiml_dial("+12027336386")
-    when "4"
-      twiml_dial("+12027336637")
-    else
-      @output = "Returning to the main menu."
-      twiml_say(@output)
-    end
-  end
-
-  private
-
-  def list_planets
-    message = "To call the planet Broh doe As O G, press 2. To call the planet
-    DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To
-    go back to the main menu, press the star key."
-
+  def zipcode
     response = Twilio::TwiML::Response.new do |r|
-      r.Gather numDigits: '1', action: planets_path do |g|
-        g.Say message, voice: 'alice', language: 'en-GB', loop:3
-      end
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-022.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: street_address_path 
     end
-
     render text: response.text
   end
 
-  def twiml_say(phrase, exit = false)
-    # Respond with some TwiML and say something.
-    # Should we hangup or go back to the main menu?
+  def street_address
     response = Twilio::TwiML::Response.new do |r|
-      r.Say phrase, voice: 'alice', language: 'en-GB'
-      if exit 
-        r.Say "Thank you for calling the ET Phone Home Service - the
-        adventurous alien's first choice in intergalactic travel."
-        r.Hangup
-      else
-        r.Redirect welcome_path
-      end
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-024.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: city_path
     end
-
     render text: response.text
   end
 
-  def twiml_dial(phone_number)
+  def city
     response = Twilio::TwiML::Response.new do |r|
-      r.Dial phone_number
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-025.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: state_path
     end
+    render text: response.text
+  end
 
+
+  def state
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-026.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: telephone_path
+    end
+    render text: response.text
+  end
+
+
+  def telephone
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-028.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: email_path
+    end
+    render text: response.text
+  end
+
+  def email
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-030.mp3"
+      r.Record finishOnKey: "#", playBeep: true, maxLength: '20', CallStatus: 'in-progress', action: thankyou_path
+    end
+    render text: response.text
+  end
+
+
+  def thankyou
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play "http://75.119.204.130/ivr/english_prompts/STE-037.mp3"
+      r.Hangup 
+    end
     render text: response.text
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,17 @@ Rails.application.routes.draw do
   # webhook for your Twilio number
   match 'ivr/welcome' => 'twilio#ivr_welcome', via: [:get, :post], as: 'welcome'
 
-  # callback for user entry
-  match 'ivr/selection' => 'twilio#menu_selection', via: [:get, :post], as: 'menu'
-
   # callback for planet entry
   match 'ivr/planets' => 'twilio#planet_selection', via: [:get, :post], as: 'planets'
-
   match 'ivr/first_name' => 'twilio#first_name', via: [:get, :post], as: 'first_name'
   match 'ivr/last_name' => 'twilio#last_name', via: [:get, :post], as: 'last_name'
+  match 'ivr/zipcode' => 'twilio#zipcode', via: [:get, :post], as: 'zipcode'
+  match 'ivr/street_address' => 'twilio#street_address', via: [:get, :post], as: 'street_address'
+  match 'ivr/city' => 'twilio#city', via: [:get, :post], as: 'city'
+  match 'ivr/state' => 'twilio#state', via: [:get, :post], as: 'state'
+  match 'ivr/telephone' => 'twilio#telephone', via: [:get, :post], as: 'telephone'
+  match 'ivr/email' => 'twilio#email', via: [:get, :post], as: 'email'
+  match 'ivr/city' => 'twilio#city', via: [:get, :post], as: 'city'
+  match 'ivr/thankyou' => 'twilio#thankyou', via: [:get, :post], as: 'thankyou'
 
 end
